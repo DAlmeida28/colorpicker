def6e1c4c5f1b2a5dedd920666e538256227e925
Daniel Almeida <almeida.danny@gmail.com>
2025-02-03 16:32:45 -0600

    add styling

--- src/ColorButton.jsx
+++ src/ColorButton.jsx
@@ -1,7 +1,7 @@
 const ColorButton = ({colorName, setSelectedColor}) => {
   return (
     <button onClick={() => setSelectedColor(colorName)}>{colorName}</button>
-  )
+ )
 }
 
 export default ColorButton;
